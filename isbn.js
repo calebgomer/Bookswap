298f2a23e1ffb0b02576036279e65fe9c8281bec
Caleb Gomer <calebgomer@gmail.com>
2014-04-03 04:54:52 -0400

    rough draft for isbn import modal

--- isbn.js
+++ isbn.js
@@ -20,7 +20,7 @@ function getIsbn13s() {
     // var isbnStuff = textToCheck.match(isbnRegex13);
     // var isbn = isbnStuff[0].substring(1, isbnStuff[0].length - 1);
 
-    console.log('stuff', isbn);
+    // console.log('stuff', isbn);
     if (isbnStart === -1) {
       console.log('done');
       console.log(isbns);
@@ -51,3 +51,28 @@ function getIsbn10s() {
     start += isbnStart + 11;
   }
 }
+/*
+<div class="modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h4 class="modal-title">Book Swap Textbook Import</h4>
+      </div>
+      <div class="modal-body">
+        <h4>We'll get more information like titles, authors, images, etc. when we get back to Book Swap.</h4>
+        <ul>
+          <li>9780077514488</li>
+          <li>9781429246606</li>
+          <li>9780471295051</li>
+          <li>9780895827487</li>
+        </ul>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Import Into Book Swap</button>
+      </div>
+    </div>
+  </div>
+</div>
+*/
