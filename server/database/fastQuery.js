cb10c4606235cc5aa0dc6cd514345009839ac9e0
Caleb Gomer <calebgomer@gmail.com>
2014-04-05 23:10:10 -0400

    fast query now works

--- server/database/fastQuery.js
+++ server/database/fastQuery.js
@@ -1,14 +1,17 @@
 var myUtils = require('../myUtils');
 
 myUtils.getDbClient(function (err) {
-  console.error('woah...', err);
+  console.error('Woah...', err);
 }, function(client, done) {
-  var query = process.argv[2];
-  console.log('query\n', query);
+  var query = process.argv.slice(2)[0];
   client.query(query, undefined, function(err, result) {
-    console.error('errors\n', err);
-    console.log('\nresults\n', result);
     done();
     myUtils.endDbConnection();
+    console.log('Ran command:\n', query);
+    if (err) {
+      console.error('Oops, errors...', err);
+    } else {
+      console.log('Successfully '+result.command+'ED', result.rowCount, 'rows, resulting rows:\n', result.rows);
+    }
   });
 });
